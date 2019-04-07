d7b7c0ff71682566083a843d4f3e0b5ef391b5db
Tony Ryan <Anthony.J.Ryan86@gmail.com>
2019-04-07 10:12:34 -0400

    saving: 42.28 of youtube video https://www.youtube.com/watch?v=H5fGvOK9nIc

---
+++ SPA_List/SPA_List/Scripts/app.js
@@ -0,0 +1,22 @@
+﻿var currentList = {};
+
+function createShoppingList() {
+    currentList.name = $("#shoppingListName").val();
+
+    //Web Service Call
+
+    $("#shoppingListTitle").html(currentList.name);
+    $("#shoppingListItems").empty();
+
+    $("#createListDiv").hide();
+    $("#shoppingListDiv").show();
+
+
+
+
+}
+
+
+$(document).ready(function () {
+    console.info("ready")
+});
