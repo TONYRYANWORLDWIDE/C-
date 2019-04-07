ae00c37ef210812fdc4f8a7173eefd005e764ae7
Tony Ryan <Anthony.J.Ryan86@gmail.com>
2019-04-07 14:14:36 -0400

    save at 1hr 15 seconds of tutorial

--- SPA_List/SPA_List/Scripts/app.js
+++ SPA_List/SPA_List/Scripts/app.js
@@ -2,6 +2,7 @@
 
 function createShoppingList() {
     currentList.name = $("#shoppingListName").val();
+    currentList.items = new Array();
 
     //Web Service Call
 
@@ -10,12 +11,43 @@ function createShoppingList() {
 
     $("#createListDiv").hide();
     $("#shoppingListDiv").show();
+}
 
+function addItem() {
+    var newItem = {};
+    newItem.name = $("#newItemName").val();
+    currentList.items.push(newItem);
+    console.info(currentList);
 
+    drawItems();
+}
 
+function drawItems() {
+    var $list = $("#shoppingListItems").empty();
+
+    for (var i = 0; i < currentList.items.length; i++) {
+        var currentItem = currentList.items[i];
+        var $li = $("<li>").html(currentItem.name)
+            .attr("id", "item_" + i);
+        var $deleteBtn =
+            $("<button onClick= 'deleteItem(" + i + ")'>D</button>").appendTo($li);
+        var $checkBtn =
+            $("<button onClick=  'checkItem(" + i + ")'>C</button>").appendTo($li);
+
+        $li.appendTo($list);
+
+    }
 
 }
 
+function deleteItem(index) {
+    currentList.items.splice(index, 1)
+    drawItems();
+}
+
+function checkItem(index) {
+    $("#item_" + index).addClass("checked");
+}
 
 $(document).ready(function () {
     console.info("ready")
