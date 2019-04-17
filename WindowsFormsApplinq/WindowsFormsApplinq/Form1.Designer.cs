7cf8ef06a13e59dbe1ac0926c9db5a45271ccf04
Tony Ryan <Anthony.J.Ryan86@gmail.com>
2019-04-17 07:45:04 -0400

    insert and delete

--- WindowsFormsApplinq/WindowsFormsApplinq/Form1.Designer.cs
+++ WindowsFormsApplinq/WindowsFormsApplinq/Form1.Designer.cs
@@ -28,8 +28,13 @@
         /// </summary>
         private void InitializeComponent()
         {
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle7 = new System.Windows.Forms.DataGridViewCellStyle();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle8 = new System.Windows.Forms.DataGridViewCellStyle();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle9 = new System.Windows.Forms.DataGridViewCellStyle();
             this.DataGrid = new System.Windows.Forms.DataGridView();
             this.btnSubmit = new System.Windows.Forms.Button();
+            this.btInsert = new System.Windows.Forms.Button();
+            this.btDelete = new System.Windows.Forms.Button();
             ((System.ComponentModel.ISupportInitialize)(this.DataGrid)).BeginInit();
             this.SuspendLayout();
             // 
@@ -37,9 +42,33 @@
             // 
             this.DataGrid.AllowUserToOrderColumns = true;
             this.DataGrid.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
+            dataGridViewCellStyle7.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleLeft;
+            dataGridViewCellStyle7.BackColor = System.Drawing.SystemColors.Control;
+            dataGridViewCellStyle7.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            dataGridViewCellStyle7.ForeColor = System.Drawing.SystemColors.WindowText;
+            dataGridViewCellStyle7.SelectionBackColor = System.Drawing.SystemColors.Highlight;
+            dataGridViewCellStyle7.SelectionForeColor = System.Drawing.SystemColors.HighlightText;
+            dataGridViewCellStyle7.WrapMode = System.Windows.Forms.DataGridViewTriState.True;
+            this.DataGrid.ColumnHeadersDefaultCellStyle = dataGridViewCellStyle7;
             this.DataGrid.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            dataGridViewCellStyle8.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleLeft;
+            dataGridViewCellStyle8.BackColor = System.Drawing.SystemColors.Window;
+            dataGridViewCellStyle8.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            dataGridViewCellStyle8.ForeColor = System.Drawing.SystemColors.ControlText;
+            dataGridViewCellStyle8.SelectionBackColor = System.Drawing.SystemColors.Highlight;
+            dataGridViewCellStyle8.SelectionForeColor = System.Drawing.SystemColors.HighlightText;
+            dataGridViewCellStyle8.WrapMode = System.Windows.Forms.DataGridViewTriState.False;
+            this.DataGrid.DefaultCellStyle = dataGridViewCellStyle8;
             this.DataGrid.Location = new System.Drawing.Point(61, 33);
             this.DataGrid.Name = "DataGrid";
+            dataGridViewCellStyle9.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleLeft;
+            dataGridViewCellStyle9.BackColor = System.Drawing.SystemColors.Control;
+            dataGridViewCellStyle9.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            dataGridViewCellStyle9.ForeColor = System.Drawing.SystemColors.WindowText;
+            dataGridViewCellStyle9.SelectionBackColor = System.Drawing.SystemColors.Highlight;
+            dataGridViewCellStyle9.SelectionForeColor = System.Drawing.SystemColors.HighlightText;
+            dataGridViewCellStyle9.WrapMode = System.Windows.Forms.DataGridViewTriState.True;
+            this.DataGrid.RowHeadersDefaultCellStyle = dataGridViewCellStyle9;
             this.DataGrid.Size = new System.Drawing.Size(583, 339);
             this.DataGrid.TabIndex = 0;
             this.DataGrid.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.DataGridView1_CellContentClick);
@@ -57,11 +86,33 @@
             this.btnSubmit.UseVisualStyleBackColor = false;
             this.btnSubmit.Click += new System.EventHandler(this.BtnSubmit_Click);
             // 
+            // btInsert
+            // 
+            this.btInsert.Location = new System.Drawing.Point(472, 393);
+            this.btInsert.Name = "btInsert";
+            this.btInsert.Size = new System.Drawing.Size(109, 45);
+            this.btInsert.TabIndex = 2;
+            this.btInsert.Text = "Insert";
+            this.btInsert.UseVisualStyleBackColor = true;
+            this.btInsert.Click += new System.EventHandler(this.BtInsert_Click);
+            // 
+            // btDelete
+            // 
+            this.btDelete.Location = new System.Drawing.Point(335, 393);
+            this.btDelete.Name = "btDelete";
+            this.btDelete.Size = new System.Drawing.Size(93, 45);
+            this.btDelete.TabIndex = 3;
+            this.btDelete.Text = "Delete";
+            this.btDelete.UseVisualStyleBackColor = true;
+            this.btDelete.Click += new System.EventHandler(this.BtDelete_Click);
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btDelete);
+            this.Controls.Add(this.btInsert);
             this.Controls.Add(this.btnSubmit);
             this.Controls.Add(this.DataGrid);
             this.Name = "Form1";
@@ -76,6 +127,8 @@
 
         private System.Windows.Forms.DataGridView DataGrid;
         private System.Windows.Forms.Button btnSubmit;
+        private System.Windows.Forms.Button btInsert;
+        private System.Windows.Forms.Button btDelete;
     }
 }
 
--- WindowsFormsApplinq/WindowsFormsApplinq/Form1.cs
+++ WindowsFormsApplinq/WindowsFormsApplinq/Form1.cs
@@ -76,5 +76,69 @@ namespace WindowsFormsApplinq
 
             
         }
+
+        private void BtInsert_Click(object sender, EventArgs e)
+        {
+
+
+            string TheBill = "";
+            DataClasses1DataContext DC = new DataClasses1DataContext();
+
+            MonthlyBill Abill = new MonthlyBill();
+            int rowindex = DataGrid.CurrentRow.Index;
+            TheBill = DataGrid.Rows[rowindex].Cells[0].Value.ToString();
+
+            var update = from b in DC.MonthlyBills
+                         where b.BILL == TheBill
+                         select b;
+
+
+            Abill.BILL = Convert.ToString(DataGrid.Rows[rowindex].Cells[0].Value);
+            Abill.COST = Convert.ToSingle(DataGrid.Rows[rowindex].Cells[1].Value);
+            Abill.Date = Convert.ToString(DataGrid.Rows[rowindex].Cells[2].Value);
+            DC.SubmitChanges(); // here will submitchanges function call and queries will automatic call.
+
+
+            DC.MonthlyBills.InsertOnSubmit(Abill);//InsertOnSubmit queries will automatic call thats the data context class handle it.
+
+            DC.SubmitChanges();
+
+            MessageBox.Show("Saved");
+
+            rowindex = 0;
+            Refresh();
+
+            
+        }
+
+        private void BtDelete_Click(object sender, EventArgs e)
+        {
+
+
+            string TheBill = "";
+            DataClasses1DataContext DC = new DataClasses1DataContext();
+
+            MonthlyBill Abill = new MonthlyBill();
+            int rowindex = DataGrid.CurrentRow.Index;
+            TheBill = DataGrid.Rows[rowindex].Cells[0].Value.ToString();
+
+            var delete = from b in DC.MonthlyBills
+                         where b.BILL == TheBill
+                         select b;
+
+            DC.MonthlyBills.DeleteAllOnSubmit(delete);// DeleteAllOnSubmit function will call and queries will automatic call thats the data context class handle it.
+
+            DC.SubmitChanges();
+
+            // SI = SDCD1.StudentInfos.Single(c => c.Id == iid);
+
+            rowindex = 0;
+
+            MessageBox.Show("deleted");
+
+            Refresh();
+
+            
+        }
     }
 }
