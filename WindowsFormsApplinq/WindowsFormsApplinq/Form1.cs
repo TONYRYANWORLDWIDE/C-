1acb2a365b182702df80e32d713086320aec2753
Tony Ryan <Anthony.J.Ryan86@gmail.com>
2019-04-18 07:06:58 -0400

    cache

--- WindowsFormsApplinq/WindowsFormsApplinq/Form1.cs
+++ WindowsFormsApplinq/WindowsFormsApplinq/Form1.cs
@@ -16,31 +16,21 @@ namespace WindowsFormsApplinq
         public Form1()
         {
             InitializeComponent();
-            //DataGrid.AutoResizeColumns(DataGridViewAutoSizeColumnsMode.Fill);
             DataClasses1DataContext db = new DataClasses1DataContext();
             var Bills = from b in db.MonthlyBills
                         //where b.BILL == "Dance"
                         select b;
-
-            //DataGrid.DataSource = Bills.ToList();
-            DataGrid.DataSource = db.MonthlyBills;
-            
-
-
-
+            DataGrid.DataSource = db.MonthlyBills;        
+                       
         }
 
         private void DataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
         {
             DataClasses1DataContext db = new DataClasses1DataContext();
-
             MonthlyBill Abill = new MonthlyBill();
             var d = db.GetChangeSet();
-
             MessageBox.Show(d.ToString());
-
             db.SubmitChanges();
-
         }
 
         private void BtnSubmit_Click(object sender, EventArgs e)
@@ -48,7 +38,6 @@ namespace WindowsFormsApplinq
 
                 string TheBill = "";
                 DataClasses1DataContext DC = new DataClasses1DataContext();
-
                 MonthlyBill Abill = new MonthlyBill(); 
                 int rowindex = DataGrid.CurrentRow.Index; 
                 TheBill = DataGrid.Rows[rowindex].Cells[0].Value.ToString();
@@ -63,7 +52,7 @@ namespace WindowsFormsApplinq
                     row.BILL = Convert.ToString(DataGrid.Rows[rowindex].Cells[0].Value);
                     row.COST = Convert.ToSingle(DataGrid.Rows[rowindex].Cells[1].Value);
                     row.Date = Convert.ToString(DataGrid.Rows[rowindex].Cells[2].Value);
-                    DC.SubmitChanges(); // here will submitchanges function call and queries will automatic call.
+                    DC.SubmitChanges(); 
                 }
 
                 MessageBox.Show("Updated");
@@ -78,7 +67,6 @@ namespace WindowsFormsApplinq
 
             string TheBill = "";
             DataClasses1DataContext DC = new DataClasses1DataContext();
-
             MonthlyBill Abill = new MonthlyBill();
             int rowindex = DataGrid.CurrentRow.Index;
             TheBill = DataGrid.Rows[rowindex].Cells[0].Value.ToString();
@@ -87,23 +75,17 @@ namespace WindowsFormsApplinq
                          where b.BILL == TheBill
                          select b;
 
-
             Abill.BILL = Convert.ToString(DataGrid.Rows[rowindex].Cells[0].Value);
             Abill.COST = Convert.ToSingle(DataGrid.Rows[rowindex].Cells[1].Value);
             Abill.Date = Convert.ToString(DataGrid.Rows[rowindex].Cells[2].Value);
-            DC.SubmitChanges(); // here will submitchanges function call and queries will automatic call.
+            DC.SubmitChanges(); 
 
 
-            DC.MonthlyBills.InsertOnSubmit(Abill);//InsertOnSubmit queries will automatic call thats the data context class handle it.
-
+            DC.MonthlyBills.InsertOnSubmit(Abill);
             DC.SubmitChanges();
-
             MessageBox.Show("Saved");
-
             rowindex = 0;
-            Refresh();
-
-            
+            Refresh();            
         }
 
         private void BtDelete_Click(object sender, EventArgs e)
@@ -112,7 +94,6 @@ namespace WindowsFormsApplinq
 
             string TheBill = "";
             DataClasses1DataContext DC = new DataClasses1DataContext();
-
             MonthlyBill Abill = new MonthlyBill();
             int rowindex = DataGrid.CurrentRow.Index;
             TheBill = DataGrid.Rows[rowindex].Cells[0].Value.ToString();
@@ -121,16 +102,11 @@ namespace WindowsFormsApplinq
                          where b.BILL == TheBill
                          select b;
 
-            DC.MonthlyBills.DeleteAllOnSubmit(delete);// DeleteAllOnSubmit function will call and queries will automatic call thats the data context class handle it.
-
+            DC.MonthlyBills.DeleteAllOnSubmit(delete);
             DC.SubmitChanges();
 
-            // SI = SDCD1.StudentInfos.Single(c => c.Id == iid);
-
             rowindex = 0;
-
             MessageBox.Show("deleted");
-
             Refresh();
 
             
